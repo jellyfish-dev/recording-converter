71bcad86f9429167a9b15dca53dae3820059976a
Radosław Szuma <radoslawszuma@gmail.com>
2024-03-15 11:08:51 +0100

    Modify runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,6 @@
 import Config
 
-if config_env() != :test do
+if config_env() != :test and Application.fetch_env(:recording_converter, :bucket_name) != nil do
   config :recording_converter,
     bucket_name: System.fetch_env!("BUCKET_NAME"),
     input_dir_path: System.fetch_env!("DIRECTORY_PATH"),
