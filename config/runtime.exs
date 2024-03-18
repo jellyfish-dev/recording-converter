21b95e68faf7473622f5b8b23d44cb894d0f97aa
Radosław Szuma <radoslawszuma@gmail.com>
2024-03-18 11:36:24 +0100

    Modify runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,15 +1,22 @@
 import Config
 
+config :recording_converter,
+  compositor_path: System.get_env("COMPOSITOR_PATH")
+
 if config_env() != :test do
   config :recording_converter,
     bucket_name: System.fetch_env!("BUCKET_NAME"),
     report_path: System.fetch_env!("REPORT_PATH"),
-    output_dir_path: System.fetch_env!("OUTPUT_DIRECTORY_PATH"),
-    compositor_path: System.get_env("COMPOSITOR_PATH")
+    output_dir_path: System.fetch_env!("OUTPUT_DIRECTORY_PATH")
 
   access_key_id = System.get_env("AWS_S3_ACCESS_KEY_ID")
   secret_access_key = System.get_env("AWS_S3_SECRET_ACCESS_KEY")
   region = System.get_env("AWS_S3_REGION")
+
+  unless is_nil(region) do
+    config :ex_aws,
+      region: region
+  end
 
   unless is_nil(access_key_id) or is_nil(secret_access_key) do
     config :ex_aws,
