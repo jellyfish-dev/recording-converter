27a0599341fcbeabef60096e6d65e168ecd12b2c
Radosław Szuma <radoslawszuma@gmail.com>
2024-03-19 09:03:38 +0100

    Fix config mox

--- config/test.exs
+++ config/test.exs
@@ -4,8 +4,7 @@ config :recording_converter,
   bucket_name: "bucket",
   report_path: "test_path/report.json",
   output_dir_path: "output/",
-  compositor_path: System.get_env("COMPOSITOR_PATH"),
-  terminator: RecordingConverter.TerminatorMock
+  compositor_path: System.get_env("COMPOSITOR_PATH")
 
 config :ex_aws,
   access_key_id: "dummy",
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,6 @@
-Mox.defmock(ExAws.Request.HttpMock, for: ExAws.Request.HttpClient)
 Mox.defmock(RecordingConverter.TerminatorMock, for: RecordingConverter.Terminator)
+Application.put_env(:recording_converter, :terminator, RecordingConverter.TerminatorMock)
+
+Mox.defmock(ExAws.Request.HttpMock, for: ExAws.Request.HttpClient)
+
 ExUnit.start(capture_log: true)
