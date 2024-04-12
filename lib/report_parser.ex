38d95616b80e9fea60e23b8afa1583f841cf57a5
Radosław Szuma <radoslawszuma@gmail.com>
2024-04-12 11:38:25 +0200

    Return with unregsiter_output_actions

--- lib/report_parser.ex
+++ lib/report_parser.ex
@@ -20,11 +20,11 @@ defmodule RecordingConverter.ReportParser do
     tracks_actions = get_track_actions(tracks)
 
     update_scene_notifications = create_update_scene_notifications(tracks_actions)
-    # unregister_output_actions = generate_unregister_output_actions(tracks_actions)
+    unregister_output_actions = generate_unregister_output_actions(tracks_actions)
     # unregister_input_actions = generate_unregister_input_actions(tracks_actions)
 
     # update_scene_notifications ++ unregister_input_actions ++ unregister_output_actions
-    update_scene_notifications
+    update_scene_notifications ++ unregister_output_actions
   end
 
   defp get_report(bucket_name, report_path) do
@@ -62,6 +62,36 @@ defmodule RecordingConverter.ReportParser do
     |> then(fn {actions, _acc} -> actions end)
   end
 
+  defp generate_unregister_output_actions(track_actions) do
+    {audio_end_timestamp, video_end_timestamp} = get_audio_and_video_end_timestamp(track_actions)
+
+    [
+      Compositor.schedule_unregister_audio_output(audio_end_timestamp),
+      Compositor.schedule_unregister_video_output(video_end_timestamp)
+    ]
+    |> Enum.reject(&is_nil(&1))
+  end
+
+  # defp generate_unregister_input_actions(track_actions) do
+  #   track_actions
+  #   |> Enum.filter(fn {atom, _track, _offset} -> atom == :end end)
+  #   |> Enum.map(fn {_atom, track, offset} ->
+  #     Compositor.schedule_unregister_input(offset, track.id)
+  #   end)
+  # end
+
+  defp get_audio_and_video_end_timestamp(track_actions) do
+    {audio_tracks, video_tracks} =
+      Enum.split_with(track_actions, fn {_atom, track, _timestamp} ->
+        track["type"] == "audio"
+      end)
+
+    audio_end_timestamp = calculate_end_timestamp(audio_tracks)
+    video_end_timestamp = calculate_end_timestamp(video_tracks)
+
+    {audio_end_timestamp || video_end_timestamp, video_end_timestamp || audio_end_timestamp}
+  end
+
   defp calculate_track_duration(track) do
     clock_rate_ms = div(track["clock_rate"], 1_000)
 
@@ -71,5 +101,14 @@ defmodule RecordingConverter.ReportParser do
     (difference_in_milliseconds - @delta_timestamp_milliseconds)
     |> Membrane.Time.milliseconds()
     |> Membrane.Time.as_nanoseconds(:round)
+  end
+
+  defp calculate_end_timestamp(tracks) do
+    if Enum.count(tracks) > 0 do
+      {_atom, _video_track, timestamp} = Enum.at(tracks, -1)
+      timestamp
+    else
+      nil
+    end
   end
 end
