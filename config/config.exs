3e0159e03f0d042973f822601477b404be5f4d9f
Radosław Szuma <radoslawszuma@gmail.com>
2024-05-08 11:36:32 +0200

    Remove logger config

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,10 @@ import Config
 
 config :recording_converter, terminator: RecordingConverter.Terminator
 
-config :logger,
-  backends: [:console],
-  compile_time_purge_matching: [
-    [level_lower_than: :info]
-  ]
+# config :logger,
+#   backends: [:console],
+#   compile_time_purge_matching: [
+#     [level_lower_than: :info]
+#   ]
 
 import_config "#{config_env()}.exs"
