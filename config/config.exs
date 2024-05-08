1a9d671af9ba522f70e15b05660b339317c68348
Radosław Szuma <radoslawszuma@gmail.com>
2024-05-08 11:20:12 +0200

    Return purge of debug logs

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ config :recording_converter, terminator: RecordingConverter.Terminator
 config :logger,
   backends: [:console],
   compile_time_purge_matching: [
-    # [level_lower_than: :info]
+    [level_lower_than: :info]
   ]
 
 import_config "#{config_env()}.exs"
