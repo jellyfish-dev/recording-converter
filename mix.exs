cc7cb5214a8cdf22e90b628912156a4ce293f11a
Radosław Szuma <56085570+Rados13@users.noreply.github.com>
2024-05-10 15:56:04 +0200

    Bump compositor and enable debug logs (#12)
    * Bump compositor and enable debug logs
    
    * Return purge of debug logs
    
    * Change package name
    
    * Remove logger config
    
    * Change encoder preset to veryfast
    
    * Remove debug logs

--- mix.exs
+++ mix.exs
@@ -62,9 +62,9 @@ defmodule RecordingConverter.Mixfile do
       {:membrane_opus_plugin, "~> 0.20.0"},
       {:membrane_http_adaptive_stream_plugin, "~> 0.18.3"},
       {:jason, "~> 1.0"},
-      {:membrane_video_compositor_plugin,
-       github: "membraneframework/membrane_video_compositor_plugin",
-       ref: "52326eaa0920bef84e0672e19ab0b0a58266dca8"},
+      {:membrane_live_compositor_plugin,
+       github: "membraneframework/membrane_live_compositor_plugin",
+       ref: "c8a5d0172f6944eaa618003ddbf365fd557dfe16"},
 
       # aws deps
       {:ex_aws, "~> 2.1"},
